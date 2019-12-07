ab87b76f67a03b5369a4aac53d4ccbab849619b7
Mariela <mbonilla.guti@gmail.com>
2019-12-07 16:55:03 -0600

    add objects

---
+++ Ditsodos/Assets/CodeC#/Objetos/Defenses/Catapult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
